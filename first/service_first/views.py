c10961c197cac17973ecf584af5ec71843c3bb1b
TimofeyBelikov <belikov-tima@Mail.ru>
2022-12-22 13:08:20 +0300

    JSON Renderer

--- first/service_first/views.py
+++ first/service_first/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
+from rest_framework import renderers
 from rest_framework.viewsets import ModelViewSet
 
 from service_first.models import *
@@ -10,10 +11,13 @@ from service_first.serializers import *
 class JobAPIFull(ModelViewSet):
     queryset = Job.objects.all()
     serializer_class = JobFullSerializer
+    renderer_classes = [renderers.JSONRenderer]
 
 class StatusAPIFull(ModelViewSet):
     queryset = Status.objects.all()
     serializer_class = StatusfullSerializer
+    renderer_classes = [renderers.JSONRenderer]
+
 class HealthCheckView(View):
     def get(self, request, *args, **kwargs):
         return HttpResponse("work!")
