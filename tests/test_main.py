587e7c2c8054fc25ad4380d055f1d60c385b626f
TimofeyBelikov <belikov-tima@Mail.ru>
2022-12-22 12:11:33 +0300

    Add tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,5 +3,20 @@ import requests
 api_url = 'http://localhost:8000'
 
 def test_healthcheck():
-    response = requests.get('http://localhost:8000')
+    response = requests.get(f'{api_url}/__health')
     assert response.status_code == 200
+def test_emptyTablecheck():
+    response = requests.get(f'{api_url}/status')
+    assert response.status_code == 200
+    assert len((response.json())) == 1
+def test_create():
+    description = "test_create description"
+    body_text = "test_create body"
+    code = 1
+    body = {"description": description, "body": body_text, "code": code}
+    response = requests.post(f'{api_url}/status', json=body)
+    assert response.status_code == 200
+    assert response.json().get('description') == description
+    assert response.json().get('body') == body_text
+    assert response.json().get('code') == code
+
